a0a6310892b423d24329c7bb5e3454068a1879ec
OxCart00 <107891625+OxCart00@users.noreply.github.com>
2023-06-06 15:21:21 -0600

    done

---
+++ app.js
@@ -0,0 +1,4 @@
+const greetings = require('./greetings')
+
+console.log(greetings.hi("Hola chicharito"))
+console.log(greetings.bye("Adios chicharito"))
---
+++ greetings.js
@@ -0,0 +1,13 @@
+// Módulo del saludo
+function hi(greeting) {
+  return greeting;
+}
+// Módulo de despedida
+function bye(goodbye) {
+  return goodbye;
+}
+// Exportación
+module.exports ={
+  hi:hi,
+  bye:bye
+}
